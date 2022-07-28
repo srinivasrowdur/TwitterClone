3e73727f7d18e12ba0ba09ff7213c20e6b77c2a0
srinivasrowdur <99046278+srinivasrowdur@users.noreply.github.com>
2022-07-28 01:15:54 +0100

    Initial Commit

---
+++ TwitterClone/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TwitterClone
+//
+//  Created by Srinivas Rowdur on 7/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Srinivas Rowdur on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
